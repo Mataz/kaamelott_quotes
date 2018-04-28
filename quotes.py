6383a443804086af5e7fbb165fb75bb78cca276f
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-04-28 19:27:47 +0200

    Added arthur()

--- quotes.py
+++ quotes.py
@@ -5,17 +5,48 @@ import bs4
 
 
 def perceval():
-    source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Perceval').text
-    soup = bs4.BeautifulSoup(source, 'lxml')
+    perceval_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Perceval').text
+    soup = bs4.BeautifulSoup(perceval_source, 'lxml')
 
     quote = soup.find_all('div', class_='citation')
     quotes = [x.text for x in quote]
 
+    print('\n Citations de Perceval \n')
     for x in quotes:
-        print(str(x))
+        print(str(x) + '\n')
 
 
 perceval()
+
+
+def karadoc():
+    karadoc_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Karadoc').text
+    soup = bs4.BeautifulSoup(karadoc_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    print('\n Citations de Karadoc \n')
+    for x in quotes:
+        print(str(x) + '\n')
+
+
+karadoc()
+
+
+def arthur():
+    arthur_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Arthur').text
+    soup = bs4.BeautifulSoup(arthur_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    print('\n Citations d\'Arthur \n')
+    for x in quotes:
+        print(str(x) + '\n')
+
+
+arthur()
 
 
 
