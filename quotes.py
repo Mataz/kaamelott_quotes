a224f97e7735221845b40781177b06a248f7e143
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-04-28 19:19:10 +0200

    updated quotes.py

--- quotes.py
+++ quotes.py
@@ -4,3 +4,18 @@ import requests
 import bs4
 
 
+def perceval():
+    source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Perceval').text
+    soup = bs4.BeautifulSoup(source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    for x in quotes:
+        print(str(x))
+
+
+perceval()
+
+
+
