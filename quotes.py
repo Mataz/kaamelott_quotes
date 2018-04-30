64466849a261bcdeabd816515b00a1d117b217a0
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-04-30 20:06:59 +0200

    Added père_blaise(), bohort(), gauvain(), guenièvre(), guethenoc()

--- quotes.py
+++ quotes.py
@@ -98,3 +98,100 @@ def leodagan():
 
 
 leodagan()
+
+
+def père_blaise():
+    père_blaise_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/P%C3%A8re_Blaise').text
+    soup = bs4.BeautifulSoup(père_blaise_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    ref = soup.find_all('div', class_='ref')
+    refs = [x.text for x in ref]
+
+    print('\n Citations de Père Blaise \n')
+    for quote, ref in zip(quotes, refs):
+        print(str(textwrap.fill(quote, width=110) + '\n') + str(
+            textwrap.fill(ref, width=90) + '\n'))
+
+
+père_blaise()
+
+
+def bohort():
+    bohort_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Bohort').text
+    soup = bs4.BeautifulSoup(bohort_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    ref = soup.find_all('div', class_='ref')
+    refs = [x.text for x in ref]
+
+    print('\n Citations de Bohort \n')
+    for quote, ref in zip(quotes, refs):
+        print(str(textwrap.fill(quote, width=110) + '\n') + str(
+            textwrap.fill(ref, width=90) + '\n'))
+
+
+bohort()
+
+
+def gauvain():
+    gauvain_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Gauvain').text
+    soup = bs4.BeautifulSoup(gauvain_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    ref = soup.find_all('div', class_='ref')
+    refs = [x.text for x in ref]
+
+    print('\n Citations de Gauvain \n')
+    for quote, ref in zip(quotes, refs):
+        print(str(textwrap.fill(quote, width=110) + '\n') + str(
+            textwrap.fill(ref, width=90) + '\n'))
+
+
+gauvain()
+
+
+def guenièvre():
+    guenièvre_source = requests.get('https://fr.wikiquote.org/wiki/Kaamelott/Gueni%C3%A8vre').text
+    soup = bs4.BeautifulSoup(guenièvre_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    ref = soup.find_all('div', class_='ref')
+    refs = [x.text for x in ref]
+
+    print('\n Citations de Guenièvre \n')
+    for quote, ref in zip(quotes, refs):
+        print(str(textwrap.fill(quote, width=110) + '\n') + str(
+            textwrap.fill(ref, width=90) + '\n'))
+
+
+guenièvre()
+
+
+def guethenoc():
+    guethenoc_source = requests.get('https://fr.wikipedia.org/wiki/Personnages_de_Kaamelott#Guethenoc').text
+    soup = bs4.BeautifulSoup(guethenoc_source, 'lxml')
+
+    quote = soup.find_all('div', class_='citation')
+    quotes = [x.text for x in quote]
+
+    ref = soup.find_all('div', class_='ref')
+    refs = [x.text for x in ref]
+
+    print('\n Citations de Guethenoc \n')
+    for quote, ref in zip(quotes, refs):
+        print(str(textwrap.fill(quote, width=110) + '\n') + str(
+            textwrap.fill(ref, width=90) + '\n'))
+
+
+guethenoc()
+
+
